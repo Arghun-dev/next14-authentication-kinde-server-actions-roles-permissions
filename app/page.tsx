edab36ef6463277b9c26aac6ea627907b3750591
Arghun <arghava.mousanezhad@utopiamusic.com>
2023-12-10 16:49:37 +0100

    Auth in Client components

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div className="p-8 flex flex-col space-y-4">
       <h1>Welcome to Kinde Authentication App</h1>
-      <div>
-        <RegisterLink className="bg-gray-300">Sign up</RegisterLink>
+      <div className="flex justify-center space-x-4">
+        <RegisterLink>Sign up</RegisterLink>
         <LoginLink>Sign in</LoginLink>
         <LogoutLink>Sign out</LogoutLink>
       </div>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface ILink {
+  path: string;
+  name: string;
+  requiresAuth?: boolean;
+}
+
+const links: ILink[] = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    requiresAuth: true,
+  },
+];
+
+export default function Sidebar() {
+  const pathname = usePathname();
+  const { user, isAuthenticated, isLoading } = useKindeBrowserClient();
+
+  return (
+    <div className="absolute left-0 top-0 bg-black h-full p-8">
+      <div className="flex flex-col">
+        {links.map((link) => {
+          if (link.requiresAuth && !isAuthenticated) {
+            return null;
+          }
+
+          return (
+            <Link
+              href={link.path}
+              key={link.path}
+              className={`bg-gray-600 mb-8 w-[200px] p-2 rounded-md cursor-pointer hover:bg-gray-500 transition-colors text-gray-200 ${
+                link.path === pathname ? "bg-gray-400" : "bg-gray-600"
+              }`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="absolute bottom-8">
+        <div className="flex justify-center">
+          <div className="animate-pulse flex justify-center">
+            {isLoading && (
+              <div className="bg-gray-300 w-[50px] h-[50px] rounded-full" />
+            )}
+          </div>
+
+          {!isLoading && isAuthenticated && (
+            <div className="flex flex-col items-center space-y-4 text-white">
+              <div>
+                {user?.given_name} {user?.family_name}
+              </div>
+              <div>{user?.email}</div>
+              {user?.picture && (
+                <Image
+                  src={user.picture}
+                  alt="Profile Image"
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                />
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
